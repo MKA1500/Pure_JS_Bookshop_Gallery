7ac18cc3f0e477f3dbe613b5e2bf3079ba299ba2
Maciej Kabsch <maciej.kabsch@lumesse.com>
2017-11-01 15:01:18 +0100

    some more

--- script.js
+++ script.js
@@ -9,22 +9,44 @@ document.addEventListener('DOMContentLoaded', function () {
     ar1[3] = "john3";
 
     console.log(ar1);
+    var arrayLength = ar1.length;
+    console.log(arrayLength);
+
+    var itemsCounter = document.getElementById("itemsCounter");
+
+    function updateItemsCounter() {
+        itemsCounter.innerHTML = "Number of items in the Array: " + arrayLength;
+    }
 
     function displayArray() {
-        var n = ar1.length;
-        console.log(n);
         var ResultsDiv = document.getElementById("ResultsDiv");
-
-        for (var i = 0; i < n; i++) {
+        var i = ResultsDiv.getElementsByTagName('div').length;
+        console.log(i);
+        for (i; i < arrayLength; i++) {
             var newDiv = document.createElement("div");
             newDiv.innerHTML = "item " +
                 (i + 1).toString() + " of " +
-                    n.toString() + " is " + ar1[i];
-            ResultsDiv.appendChild(newDiv);
+                arrayLength.toString() + " is " + ar1[i];
+            newDiv.className = "element_" + i;
+            ResultsDiv.insertBefore(newDiv, ResultsDiv.firstChild);
         }
     }
 
+    function addItemToExistingList() {
+        ar1[arrayLength] = document.getElementById("textValue").value;
+        arrayLength++;
+        document.getElementById("textValue").value = "";
+        console.log(ar1);
+
+        updateItemsCounter();
+    }
+
+    updateItemsCounter();
+
     var showArrayButton = document.getElementById("showArrayButton");
     showArrayButton.addEventListener("click", displayArray);
+
+    var addToArray = document.getElementById("addToArray");
+    addToArray.addEventListener("click", addItemToExistingList);
 
 }, false);
